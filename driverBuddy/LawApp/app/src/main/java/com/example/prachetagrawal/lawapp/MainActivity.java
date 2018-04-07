812188419ab3debdeab275f471f7936cf7466af6
LucasFerence <ference.lucas@gmail.com>
2018-04-07 18:40:43 -0400

    location

--- driverBuddy/LawApp/app/src/main/java/com/example/prachetagrawal/lawapp/MainActivity.java
+++ driverBuddy/LawApp/app/src/main/java/com/example/prachetagrawal/lawapp/MainActivity.java
@@ -6,16 +6,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private BroadcastReceiver broadcastReceiver;
+    private String curState = "defState";
 
     @Override
     protected void onResume() {
@@ -24,7 +32,19 @@ public class MainActivity extends AppCompatActivity {
             broadcastReceiver = new BroadcastReceiver() {
                 @Override
                 public void onReceive(Context context, Intent intent) {
-                    //Do something after recieving coordinates
+                    String coords = intent.getStringExtra("coordinates");
+                    String temp[] = coords.split(" ");
+                    int longitude = Integer.parseInt(temp[0]);
+                    int latitude = Integer.parseInt(temp[1]);
+
+                    String newState = findState(longitude, latitude);
+                    if (!newState.equals(curState)) {
+                        curState = newState;
+                        TextView t = findViewById(R.id.testLocation);
+                        t.setText(curState);
+                    }
+
+
                 }
             };
         }
@@ -59,6 +79,18 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    private String findState(double lng, double lat) {
+        Geocoder gcd = new Geocoder(getApplicationContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = gcd.getFromLocation(lat, lng, 1);
+            if (addresses.size() > 0)
+                return addresses.get(0).getAdminArea();
+        } catch (IOException e) {
+            return "";
+        }
+        return "";
     }
 
 }
