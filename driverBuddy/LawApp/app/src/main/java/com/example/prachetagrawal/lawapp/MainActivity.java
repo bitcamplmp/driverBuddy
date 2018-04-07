ccccbf8b2ae4fc69f7068944c26c6c4028938fe8
bitlmp <prachet@umd.edu>
2018-04-06 20:51:37 -0400

    I made string in this class

--- driverBuddy/LawApp/app/src/main/java/com/example/prachetagrawal/lawapp/MainActivity.java
+++ driverBuddy/LawApp/app/src/main/java/com/example/prachetagrawal/lawapp/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    String java;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
