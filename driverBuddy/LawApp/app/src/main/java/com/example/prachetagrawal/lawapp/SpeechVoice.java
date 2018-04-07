27e32b5ab3547e82dd9de6cde5e55c84fd0f3e8f
mpatel18 <mpatel14@terpmail.umd.edu>
2018-04-06 23:59:41 -0400

    Text to Speech

---
+++ driverBuddy/LawApp/app/src/main/java/com/example/prachetagrawal/lawapp/SpeechVoice.java
@@ -0,0 +1,26 @@
+package com.example.prachetagrawal.lawapp;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+import java.util.Locale;
+
+public class SpeechVoice {
+    public TextToSpeech reader; // Reader to take in string and read laws
+    public Context context;
+
+    public SpeechVoice(){
+        reader = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i != TextToSpeech.ERROR){
+                    reader.setLanguage(Locale.US);
+                }
+            }
+        });
+    }
+
+    public void readSentence(String sentance){
+
+    }
+}
