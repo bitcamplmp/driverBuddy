1646d12937263ea8a8176991174bea6c5e74b833
LucasFerence <ference.lucas@gmail.com>
2018-04-07 16:59:13 -0400

    Adding super complicated back buttons

--- driverBuddy/LawApp/app/src/main/java/com/example/prachetagrawal/lawapp/MainActivity.java
+++ driverBuddy/LawApp/app/src/main/java/com/example/prachetagrawal/lawapp/MainActivity.java
@@ -7,14 +7,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    public String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SpeechVoice speach = new SpeechVoice(savedInstanceState);
-        speach.readSentence(text, savedInstanceState);
+        //SpeechVoice speach = new SpeechVoice(savedInstanceState);
+        //speach.readSentence(text, savedInstanceState);
         Button mainButton = findViewById(R.id.buttonToMain);
         mainButton.setOnClickListener(new View.OnClickListener() {
             @Override
