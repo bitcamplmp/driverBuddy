126419383cca29472b8a7986dcf28a201cf8dd6e
bitlmp <prachet@umd.edu>
2018-04-07 16:44:04 -0400

    Work on location base and the databse of laws

--- driverBuddy/LawApp/app/src/main/java/com/example/prachetagrawal/lawapp/MainActivity.java
+++ driverBuddy/LawApp/app/src/main/java/com/example/prachetagrawal/lawapp/MainActivity.java
@@ -1,16 +1,51 @@
 package com.example.prachetagrawal.lawapp;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BroadcastReceiver broadcastReceiver;
 
-    String java;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(broadcastReceiver == null) {
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    //Do something after recieving coordinates
+                }
+            };
+        }
+        registerReceiver(broadcastReceiver, new IntentFilter("Location Updates"));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!runtimePermissions()) {
+            //enableService();
+        }
     }
+
+    private Boolean runtimePermissions() {
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            return true;
+        }
+        return false;
+    }
+
 }
